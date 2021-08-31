f13cc6cc54d852658221d3e47f6b72a5fdc64204
chalobon <chalobon-jaje@hotmail.com>
2021-08-31 23:41:53 +0700

    add footer component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Content from '../components/Content';
 import Gallery from '../components/Gallery';
 import Team from '../components/Team';
-
-const Contact = (props) => {
+import Footer from '../components/Footer';
+const Contact = () => {
   return (
     <div>
       <Content />
       <Team />
       <Gallery />
+      <Footer />
     </div>
   );
 };
